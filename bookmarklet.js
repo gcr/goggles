b96a2c3d8173fbb5c1615c8b7c860ec424e91ecb
Michael W <gcr@sneakygcr.net>
2010-11-26 00:40:54 -0700

    changed bookmark source to github

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,6 +2,6 @@ javascript: (function () {
     window.GOGGLE_SERVER='http://goggles.sneakygcr.net/';
     var scr = document.createElement('script');
     scr.type = 'text/javascript';
-    scr.src = 'http://localhost/goggles/goggles.js?rand='+Math.random();
+    scr.src = 'https://github.com/gcr/goggles/raw/master/goggles.js?rand='+Math.random();
     document.documentElement.appendChild(scr);
 })();
