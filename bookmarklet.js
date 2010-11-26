407ec3f1e1bb9c6731b4780ac8237f170eeec831
Michael W <gcr@sneakygcr.net>
2010-11-26 12:11:47 -0700

    fixed goggles

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,6 +2,6 @@ javascript: (function () {
     window.GOGGLE_SERVER='http://goggles.sneakygcr.net/';
     var scr = document.createElement('script');
     scr.type = 'text/javascript';
-    scr.src = 'https://github.com/gcr/goggles/raw/master/goggles.js?rand='+Math.random();
+    scr.src = 'http://goggles.sneakygcr.net/bookmarklet.js?rand='+Math.random();
     document.documentElement.appendChild(scr);
 })();
