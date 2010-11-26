99020b6bc25a6b83bb47e4b7237c346ed14ca5e7
Michael W <gcr@sneakygcr.net>
2010-11-26 12:22:28 -0700

    fixed the server page

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 javascript: (function () {
-    window.GOGGLE_SERVER='http://goggles.sneakygcr.net/';
+    window.GOGGLE_SERVER='http://goggles.sneakygcr.net/page';
     var scr = document.createElement('script');
     scr.type = 'text/javascript';
     scr.src = 'http://goggles.sneakygcr.net/bookmarklet.js?rand='+Math.random();
