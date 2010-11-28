b1f521957915c27b98ca7a151b41364bf98df462
Michael W <gcr@sneakygcr.net>
2010-11-28 14:47:16 -0700

    Revert "less console"
    Still times out freakily.
    
    This reverts commit e87e1404bc3e7f99864a2f9f5afada212d51d2a6.

--- models/async_lock.js
+++ models/async_lock.js
@@ -14,6 +14,7 @@ AsyncLock.prototype.lock = function(action) {
   // WARNING WARNING WARNING! IF YOU DO NOT CALL UNLOCK, THE LOCK WILL REMAIN
   // LOCKED. Ensure that ALL code paths eventually call unlock or you WILL run
   // into troubles.
+  console.log("Lock");
   var self = this;
   this.ops.push(action);
   if (!this.locked) {
@@ -23,6 +24,7 @@ AsyncLock.prototype.lock = function(action) {
         if (self.ops.length===0) {
           // Done!
           self.locked = false;
+          console.log("Unlock");
         } else {
           // call the first action, passing in runNext
           self.ops.shift()(unlockOrRunNext);
--- models/history.js
+++ models/history.js
@@ -16,13 +16,17 @@ History.prototype.time = function() {
 
 History.prototype.add = function(obj) {
   var now = this.time();
+  console.log("added to history,");
   if (now in this.futures) {
     // If people are waiting for us, then give them stuff.
     console.log(require('util').inspect(this.futures[now]));
     this.futures[now].forEach(function(cb) {
       clearTimeout(cb.timer);
       if (cb.cb) { // if it didn't time out
+        console.log("cb...");
         cb.cb([obj]);
+      } else {
+        console.log("stale cb");
       }
     });
     delete this.futures[now];
@@ -34,6 +38,7 @@ History.prototype.after = function(time, cb) {
   // Run the callback with all the actions that happened after the given time.
   // Block to wait for them if necessary.
 
+  console.log("waiting");
 
   if (time < this.time()) {
     // Aha! We can already fufill their request.
@@ -44,6 +49,7 @@ History.prototype.after = function(time, cb) {
     var cbData = {
         cb: cb,
         timer: setTimeout(function(){
+            console.log("timed out");
             // this gets run on timeout
             cb([]);
             cbData.cb = null;
