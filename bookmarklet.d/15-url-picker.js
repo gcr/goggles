b42433ee247fef7df6e8f24d575b90ea43317aa0
Michael W <gcr@sneakygcr.net>
2010-11-27 22:27:22 -0700

    fixed twitter urls

--- bookmarklet.d/15-url-picker.js
+++ bookmarklet.d/15-url-picker.js
@@ -1,5 +1,11 @@
 function getUrl() {
   // return a unique URL for this page
+  //
+  // due to a stupid mistake all URLs look like
+  // http://foo.com//bar instead of
+  // http://foo.com/bar like they should be.
+  // too many slashes? sorry...
+
   var l = document.location,
       base = l.protocol+"//"+l.host+"/"+l.pathname;
 
@@ -8,6 +14,12 @@ function getUrl() {
     base = base + l.search.replace(new RegExp('page__.*'), '');
     if (base[base.length-1] == '/') {
       base = base.substr(0, base.length-1);
+    }
+  } else if (l.host == 'twitter.com') {
+    // twitter uses 'pretty' URLs
+    if (l.hash.indexOf("#!")===0) {
+      base = base + (l.hash.indexOf('/')==2? l.hash.substr(3)
+                                           : l.hash.substr(2));
     }
   }
 
