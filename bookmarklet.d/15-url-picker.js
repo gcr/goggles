8accb0f125d3a6ca9a31f7817b43c5852f2bf70b
Michael W <gcr@sneakygcr.net>
2010-11-27 19:49:20 -0700

    fixed board

--- bookmarklet.d/15-url-picker.js
+++ bookmarklet.d/15-url-picker.js
@@ -5,7 +5,10 @@ function getUrl() {
 
   // special cases
   if (l.host == 'board.iamlights.com') {
-    return base + l.search;
+    base = base + l.search.replace(new RegExp('page__.*'), '');
+    if (base[base.length-1] == '/') {
+      base = base.substr(0, base.length-1);
+    }
   }
 
   return base;
