cc1ae019a3c54cc43e6e342b46a96730512a68ef
Michael W <gcr@sneakygcr.net>
2010-12-03 00:01:42 -0700

    refactored tests

--- test/common.js
+++ test/common.js
@@ -1,16 +1,26 @@
 var fs = require('fs'),
-    proc = require('child_process');
+    proc = require('child_process'),
+    Keystore = require('../models/keystore').Keystore;
 
-exports.fresh_dir = function(dir){
-  return function(){
-    var cb = this.callback;
-    proc.spawn('rm', ['-rf', dir])
-    .on('exit', function(code){
-        if (code===0) {
-          fs.mkdir(dir, 448, cb);
-        } else {
-          throw new Error("rm failed");
-        }
-      });
-  };
+exports.withFreshDir = function(dir, cb){
+  proc.spawn('rm', ['-rf', dir])
+  .on('exit', function(code){
+      if (code===0) {
+        fs.mkdir(dir, 448, cb);
+      } else {
+        throw new Error("rm failed");
+      }
+    });
+};
+
+exports.AFreshDir = function(dir, obj) {
+  obj.topic = function(){ exports.withFreshDir(dir, this.callback); };
+  return { 'In a fresh dir,': obj };
+};
+
+exports.AFreshKeystore = function(dir, obj) {
+  obj.topic = function(){ return new Keystore(dir);};
+  return exports.AFreshDir(dir, {
+      'a fresh KS': obj
+    });
 };
