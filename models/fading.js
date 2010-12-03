7b9725bc0e59365e94a4421de429376a6b5694e5
Michael W <gcr@sneakygcr.net>
2010-12-02 22:23:28 -0700

    faded webhp google

--- models/fading.js
+++ models/fading.js
@@ -3,7 +3,9 @@
 var FADE_URL = ["http://www.google.com//",
                 "http://www.facebook.com//",
                 "http://goggles.sneakygcr.net//",
-                "http://www.reddit.com//"],
+                "http://www.reddit.com//",
+                "http://www.google.com//webhp"
+              ],
     FADE_INTERVAL = 5 * 60 * 1000,
     FADE_DIFF = 0.1,
     FADE_CUTOFF_THRESH = 0.1;
