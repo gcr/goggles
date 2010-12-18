1c2ff968b8190e3b41fb7bb726aeae75fb6d7884
Michael W <gcr@sneakygcr.net>
2010-12-18 16:47:09 -0700

    better brush size pickers

--- bookmarklet.d/20-picker.js
+++ bookmarklet.d/20-picker.js
@@ -14,17 +14,19 @@ function hex2rgb(hex) {
     return {r:0,g:0,b:0};
   }
 }
-function Picker(onPickColor) {
+function Picker(onPickColor, onPickBrush) {
   var self = this;
-  this.jq = $("<div>").css({
+  this.colorsJq = $("<div>").css({
     position: "fixed",
     cursor: 'pointer',
     "z-index": "100001",
-    border: "solid 1px #000",
     top: "0",
-    left: "0"
+    left: "0",
+    width: "32px",
+    border: 'solid 1px #000'
   });
   var chosenColor = $();
+  // different brush colors!
   var colors = ["#000", "#fff", "#e50", "#fa0", "#1ba", "#e07", "#ab0"]
     .map(function(color) {
       var colorjq = $("<div>").css({"background-color": color,
@@ -34,21 +36,67 @@ function Picker(onPickColor) {
                                     'text-align': 'center',
                                     width: 32, height: 64});
       colorjq.click(function(){
-          if (colorjq == chosenColor) { return; }
+        if (colorjq == chosenColor) { return; }
           onPickColor(hex2rgb(color));
           chosenColor.text("");
           chosenColor = colorjq;
           colorjq.html("&bull;");
         });
-      colorjq.appendTo(self.jq);
+      colorjq.appendTo(self.colorsJq);
       return colorjq;
     });
   colors[0].click();
+
+  // different brush sizes!
+  var chosenBrush = $();
+  this.brushesJq = $("<div>")
+        .css({
+            position: "fixed",
+            cursor: 'pointer',
+            "z-index": "100001",
+            top: "0",
+            left: "32px",
+            'float': 'right',
+            'background-color':'#fff',
+            color:'#000',
+            margin:0
+          });
+  var brushes = [2,5,10,15,20]
+    .map(function(size) {
+        var brushJq = $("<div>")
+              .css({
+                display: 'block',
+                'float': 'left',
+                width: "32px",
+                padding:0,
+                height: "32px"
+              });
+        brushJq.append($("<div>")
+            .css({
+                position: "absolute",
+                height: size+"px",
+                'background-color': "#000",
+                top: (16-size/2)+"px",
+                width: "32px"
+              }));
+        brushJq.click(function(){
+          if (brushJq == chosenBrush) { return; }
+            onPickBrush(size);
+            chosenBrush = brushJq;
+            //chosenColor.text("");
+            //colorjq.html("&bull;");
+          });
+        brushJq.appendTo(self.brushesJq);
+        return brushJq;
+    });
+  brushes[0].click();
 }
 Picker.prototype.del = function() {
-  this.jq.fadeOut('fast', bind(this,function(){this.jq.remove();}));
+  this.colorsJq.fadeOut('fast', bind(this,function(){this.colorsJq.remove();}));
+  this.brushesJq.fadeOut('fast', bind(this,function(){this.brushesJq.remove();}));
 };
 Picker.prototype.show = function() {
-  this.jq.hide().appendTo($("body")).slideDown('medium');
+  this.brushesJq.hide().appendTo($("body")).slideDown('medium');
+  this.colorsJq.hide().appendTo($("body")).slideDown('medium');
 };
 
