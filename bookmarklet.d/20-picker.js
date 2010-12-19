ed0cabadae2cf8ac98419954cb875a4bb7448a1d
Michael W <gcr@sneakygcr.net>
2010-12-18 17:05:15 -0700

    added a border

--- bookmarklet.d/20-picker.js
+++ bookmarklet.d/20-picker.js
@@ -58,6 +58,7 @@ function Picker(onPickColor, onPickBrush) {
             left: "32px",
             'float': 'right',
             'background-color':'#fff',
+            border: 'solid 1px #000',
             color:'#000',
             margin:0
           });
